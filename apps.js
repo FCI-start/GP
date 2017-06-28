910378caf9ebac3ade9542ffb4a678ea3fd7faed
shawara <m.shawara@outlook.com>
2017-06-28 20:40:46 +0200

    Ace Editor JavaFunction

--- apps.js
+++ apps.js
@@ -181,7 +181,7 @@ var DragDrop =
                     window.ProjectManager.generateMainLayout(newObj._id);
                 }
                 else {
-                    toggle_visibility('popupBoxOnePosition');
+                    window.toggle_visibility('popupBoxOnePosition');
                 }
             });
             DragDrop.selected.style.left = DragDrop.selected.beginningPositionLeft + 'px';
